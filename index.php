23255bb5bf2bfa8a2568276b74d697082d2f79de
isaac <tasacata_sorte@yahoo.co.jp>
2019-11-30 01:02:39 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+  <meta charset="UTF-8">
+  <title>first file</title>
+  </head>
+  <body>
+    <h1>今日の日付</h1>
+    <p>今日は<?php echo date('Y年m月d日'); ?>です。</p>
+  </body>
+</html>
